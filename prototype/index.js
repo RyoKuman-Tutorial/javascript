2290dee49195a66d0c258d8caf03539136c150ac
Ryokuman <ryokuman@naver.com>
2022-11-28 01:53:25 +0900

    use prototype
    프로토타입을 사용함으로써, name 추가

--- prototype/index.js
+++ prototype/index.js
@@ -1,6 +1,11 @@
-function machine() {
+function Machine() {
   this.q = "consume";
   this.w = "snowball";
 }
 
-const nunu = new machine();
+Machine.prototype.name = "nunu";
+// Machin 의 프로로타입 변경
+
+const nunu = new Machine();
+console.log(nunu.name);
+// nunu 출력 => nunu는 name 을 가지고 있지 않지만, nunu의 부모가 name을 가지고 있으므로 출력 가능
