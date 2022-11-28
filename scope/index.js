c1a5bddb5ebf83a12cbe653de14dc85b85e0d6d2
Ryokuman <ryokuman@naver.com>
2022-11-28 16:10:41 +0900

    function ( local ) scope
    함수 ( 지역 )  스코프, 개념 정리

---
+++ scope/index.js
@@ -0,0 +1,18 @@
+var a = 1; // 전역 스코프
+
+function funcPrint1() {
+  var a = 111;
+  console.log("funcPrint1 is : " + a);
+} // 지역(함수) 스코프
+
+function funcPrint2() {
+  var a = 11;
+  function funcPrint3() {
+    console.log("funcPrint2 is :" + a);
+  }
+  funcPrint3();
+} // 지역(함수) 스코프
+
+funcPrint1(); // 111 => function(local) scope 내부에 a 가 존재하므로, 해당 a 출력
+funcPrint2(); // 11 => function(local) scope 내부에 a 가 존재하지 않으므로, scope chain 에 의해 a 를 찾을때 까지, 상위 scope 검토
+console.log("funct console.log is : " + a); // 1 => function(local) scope 내부에 a 가 존재하지 않으므로, scope chain 에 의해 a 를 찾을때 까지, 상위 scope 검토
