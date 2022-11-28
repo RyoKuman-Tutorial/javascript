108202ec9e29702a561b408122694129b51ba5f4
Ryokuman <ryokuman@naver.com>
2022-11-28 16:14:12 +0900

    block level scope
    블록 레벨 스코프 개념 정리

--- scope/index.js
+++ scope/index.js
@@ -16,3 +16,14 @@ function funcPrint2() {
 funcPrint1(); // 111 => function(local) scope 내부에 a 가 존재하므로, 해당 a 출력
 funcPrint2(); // 11 => function(local) scope 내부에 a 가 존재하지 않으므로, scope chain 에 의해 a 를 찾을때 까지, 상위 scope 검토
 console.log("funct console.log is : " + a); // 1 => function(local) scope 내부에 a 가 존재하지 않으므로, scope chain 에 의해 a 를 찾을때 까지, 상위 scope 검토
+
+function blockPrint() {
+  const b = 111;
+  console.log("blockPrint is :" + b);
+}
+
+{
+  const b = 1;
+  console.log("block console.log is : " + a);
+  blockPrint();
+}
